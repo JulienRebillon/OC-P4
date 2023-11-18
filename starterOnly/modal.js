42b96ff3acbb9122bc1f679d865cf1b8f1cc2946
JulienRebillon <julienrebillon@yahoo.fr>
2023-11-18 23:14:27 +0100

    style font-size and img

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,6 +10,7 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
@@ -20,4 +21,11 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal event
+modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
+//modalClose.addEventListener("click", closeModal);
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}

2faf0bd12b1df34d10aec0899207c30981a1b7cf
JulienRebillon <julienrebillon@yahoo.fr>
2023-11-19 14:29:09 +0100

    script

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -25,7 +25,37 @@ function launchModal() {
 modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
 //modalClose.addEventListener("click", closeModal);
 
-// close modal form
+// Close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+// launch data format verification
+form.addEventListener("submit", (event) => { 
+  event.preventDefault(); //prevents default refresh of the page on sumbit
+
+  //verification
+  addEventListener("click", )
+
+  //verification prénom
+  const balisePrenom = document.getElementById('first');
+  const valeurPrenom = balisePrenom.value;
+  //const valeurPrenom = document.getElementById('first').value;
+  if (valeurPrenom === "") {
+    console.log ('prénom vide');
+    //insertion message d'erreur champ vide.
+
+  } else { 
+      if (valeurPrenom.length <= 2) {
+        console.log ('prénom trop court');
+        //insertion message d'erreur prénom.
+      } else {
+          console.log ('prénom correctement rempli');
+      }
+    }
+
+
+
+    const validationOk = 
+
+  });
